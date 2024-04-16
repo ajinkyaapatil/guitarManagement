664fa56bbb051117b321c2fbed37f5f4746f5a1c
Ajinkya Patil <ajinkyaarvindp@sahaj.ai>
2024-04-16 22:23:13 +0530

    Add test for unavailble guitar spec case

--- src/test/kotlin/GuitarInventoryTest.kt
+++ src/test/kotlin/GuitarInventoryTest.kt
@@ -20,11 +20,10 @@ class GuitarInventoryTest {
         val guitars = guitarInventory.guitars
 
         assertEquals(1, guitars[0].serialNumber)
-        // add a function instead of ...
     }
 
     @Test
-    fun `should return a guitar when provided the serial Number`(){
+    fun `should return a guitar when provided with the serial Number`(){
         val guitarSpecs = GuitarSpecs(Builder.FENDER, Type.ELECTRIC, Wood.CEDAR, Wood.ALDER)
         val guitar = Guitar(1, 100, "New", guitarSpecs)
 
@@ -50,4 +49,21 @@ class GuitarInventoryTest {
 
         assertEquals(1, guitars[0].serialNumber)
     }
+
+    @Test
+    fun `should return an empty list when provided a unavailable guitar specification`(){
+        val guitarSpecs = GuitarSpecs(Builder.FENDER, Type.ELECTRIC, Wood.CEDAR, Wood.ALDER)
+        val guitar = Guitar(1, 100, "New", guitarSpecs)
+        val guitarInventory = GuitarInventory()
+        guitarInventory.add(guitar)
+
+
+        val requiredSpecs = GuitarSpecs(Builder.FENDER, Type.ACOUSTIC, Wood.CEDAR, Wood.ALDER)
+
+        val guitars = guitarInventory.search(requiredSpecs)
+
+        assertEquals(0, guitars.size)
+    }
+
+
 }
